0e0d5df00afa408f3c03b4069e549c1f76931949
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-10 14:00:31 +0000

    Reversed comments in chronological order

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,11 @@
 class ReviewsController < ApplicationController
   def create
-    @review.user = current_user
-    Review.create(description: params[:review][:description], rating: params[:review][:rating], product_id: params[:product_id])
+    Review.create(
+      description: params[:review][:description],
+      rating: params[:review][:rating],
+      product_id: params[:product_id],
+      user: current_user
+    )
     redirect_to "/products/#{params[:product_id]}"
   end
 end
