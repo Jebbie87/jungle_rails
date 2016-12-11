82d7155fed88db739dec7a8fd963cb9f707f69fb
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-11 19:49:04 +0000

    Added a styled comment box and put the comments section into table

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,18 @@
 class ReviewsController < ApplicationController
-
   before_filter :current_user?
   def create
     Review.create(
-      description: params[:review][:description],
+      description: params[:description],
       rating: params[:review][:rating],
       product_id: params[:product_id],
       user: current_user
     )
+    redirect_to "/products/#{params[:product_id]}"
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
     redirect_to "/products/#{params[:product_id]}"
   end
 
