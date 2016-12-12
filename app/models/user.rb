33d67e4acd0bcbc381d15f737d22bda648d4d037
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-12 22:39:43 +0000

    Tests added to test for password, first_name, last_name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
 
 # create_table :users do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,31 +6,60 @@ RSpec.describe User, type: :model do
     @user_wo_pwd = User.new(
       first_name: Faker::Superhero.name,
       last_name: Faker::Superhero.power,
-      email: 'super@powers.com'
+      email: 'test@test.COM'
     )
 
     @user = User.new(
       first_name: Faker::Superhero.name,
       last_name: Faker::Superhero.power,
-      email: 'super@powers.com',
+      email: 'TEST@TESTs.com',
       password: 'test',
-      password_confirmation: 'test123'
+      password_confirmation: 'test'
     )
   end
 
-  it "should have a filled out password field that is not a nil value" do
-    expect(@user_wo_pwd.password).to be_nil
-    should vali
+  it "should produce an error message if password field is not present" do
+    @user.password = nil
+    @user.save
+    expect(@user.errors.full_messages).to include("Password can't be blank")
   end
 
-  it "should have a filled out password_confirmation field that is not a nil value" do
-    expect(@user.password_confirmation).to be(true)
-    expect(@user_wo_pwd.password_confirmation).to be(false)
+  it "should produce an error message if password_confirmation field is not present" do
+    @user.password_confirmation = nil
+    @user.save
+    expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
   end
 
-  # it "should be matching password and password_confirmation field" do
-  #   @user.password_confirmation = 'test123'
-  #   expect(@user.password).to eq(@user.password_confirmation)
-  # end
+  it "should produce an error message if first_name field is not present" do
+    @user.first_name = nil
+    @user.save
+    expect(@user.errors.full_messages).to include("First name can't be blank")
+  end
+  it "should produce an error message if last_name field is not present" do
+    @user.last_name = nil
+    @user.save
+    expect(@user.errors.full_messages).to include("Last name can't be blank")
+  end
 
+  it "should produce an error message if email field is not present" do
+    @user.email = nil
+    @user.save
+    expect(@user.errors.full_messages).to include("Email can't be blank")
+  end
+
+  it "should not having matching emails (case insensitive) between two different users" do
+    @user.email = @user.email.downcase
+    @user_wo_pwd.email = @user_wo_pwd.email.downcase
+    @user_wo_pwd.save
+    @user.save
+    expect(@user.email).not_to eq(@user_wo_pwd.email)
+  end
+
+  it "should be matching password and password_confirmation field" do
+    expect(@user.password).to eq(@user.password_confirmation)
+  end
+
+  it "should not have a password longer or equal to 5 characters" do
+    expect(@user.password.length).to be > 5
+  end
 end
