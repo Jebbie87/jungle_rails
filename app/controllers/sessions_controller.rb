1fdba96f2885fc212ef67c610f79f33cf068e474
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-12 23:24:42 +0000

    Added the authenticate_with_credential methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
 
-    if @user &&  @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
       redirect_to :root
     else
-      # session.errors.add(:)
-      redirect_to new_session_url, :error => "test error message"
+      redirect_to new_session_url
     end
   end
+
 
   def destroy
     cart = nil
