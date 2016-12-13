c68750b29e86204aab732f9463748e223ce30eb4
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-13 21:14:40 +0000

    Added the feature test for users clicking on the details button that will bring them to the product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product detail page when they click the details button" do
+
+    # ACT
+    visit root_path
+
+    click_link("Details", :href => product_path(10))
+    sleep(5)
+
+    # DEBUG
+    save_screenshot
+
+    #VERIFY
+    expect(page).to have_css '.product-detail', count: 1
+  end
+
+end
