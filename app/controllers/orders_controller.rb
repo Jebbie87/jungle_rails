0eda1a7edd6430c63493aa81ecf5a951f0b1804b
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-14 05:34:21 +0000

    Finished adding the test for checking that a certain product quantity does not decrease while it is not in the order. Also finished implenting the method on the development to decrease quantity of a product after an order has been placed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,6 @@ class OrdersController < ApplicationController
   def create
     charge = perform_stripe_charge
     order  = create_order(charge)
-    byebug
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe 'After creation' do
     before :each do
-      @catgory1 = Category.create!(
+      @category1 = Category.create!(
         name: 'category1'
       )
 
@@ -16,14 +16,21 @@ RSpec.describe Order, type: :model do
         description: 'description1',
         price_cents: 100,
         quantity: 10,
-        category_id: 1
+        category: @category1
       )
       @product2 = Product.create!(
         name: 'product2',
         description: 'description2',
         price_cents: 200,
         quantity: 20,
-        category_id: 2
+        category: @category2
+      )
+      @product3 = Product.create!(
+        name: 'product3',
+        description: 'description3',
+        price_cents: 300,
+        quantity: 30,
+        category: @category1
       )
       # Setup at least one product that will NOT be in the order
     end
@@ -38,43 +45,74 @@ RSpec.describe Order, type: :model do
       )
       # 2. build line items on @order
       @order.line_items.new(
-        product_id: 1,
+        product: @product1,
         quantity: 2,
         item_price_cents: 100,
         total_price_cents: 200
       )
       @order.line_items.new(
-        product_id: 2,
+        product: @product2,
         quantity: 2,
         item_price_cents: 200,
         total_price_cents: 400
       )
-      # ...
       # 3. save! the order - ie raise an exception if it fails (not expected)
       @order.save!
 
-
       # 4. reload products to have their updated quantities
       if @order.save
-        expected_product_1 = Product.find(@product1.id)
-        expected_product_2 = Product.find(@product2.id)
         @order.line_items.each do |x|
-          product = Product.find(x.product.id)
+          byebug
+          product = Product.find(x.product_id)
           product.quantity -= x.quantity
           product.save
         end
-        # byebu
       end
       @product1.reload
       @product2.reload
+      @product3.reload
+      # 5. use RSpec expect syntax to assert their new quantity values
       expect(@product1.quantity).to eq(8)
       expect(@product2.quantity).to eq(18)
-      # 5. use RSpec expect syntax to assert their new quantity values
-      # ...
     end
     # pending test 2
-    # xit 'does not deduct quantity from products that are not in the order' do
+    it 'does not deduct quantity from products that are not in the order' do
       # TODO: Implement based on hints in previous test
-    # end
+      @order = Order.new(
+        total_cents: 600,
+        stripe_charge_id: 1,
+        email: 'test@test.com'
+      )
+      # 2. build line items on @order
+      @order.line_items.new(
+        product: @product1,
+        quantity: 2,
+        item_price_cents: 100,
+        total_price_cents: 200
+      )
+      @order.line_items.new(
+        product: @product2,
+        quantity: 2,
+        item_price_cents: 200,
+        total_price_cents: 400
+      )
+      # 3. save! the order - ie raise an exception if it fails (not expected)
+      @order.save!
+
+      # 4. reload products to have their updated quantities
+      if @order.save
+        @order.line_items.each do |x|
+          product = Product.find(x.product_id)
+          product.quantity -= x.quantity
+          product.save
+        end
+      end
+      @product1.reload
+      @product2.reload
+      @product3.reload
+      expect(@product1.quantity).to eq(8)
+      expect(@product2.quantity).to eq(18)
+      expect(@product3.quantity).to eq(30)
+    end
   end
 end
